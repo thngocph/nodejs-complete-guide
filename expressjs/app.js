9f05584971cf8d3245a0dd4beaf230a1adf0883f
thngocph <thngocph@gmail.com>
2022-09-21 22:06:01 +0700

    bodyParser and Router

--- expressjs/app.js
+++ expressjs/app.js
@@ -1,26 +1,27 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
 
 const app = express();
 
 // app.get: matches the entire request url
 app.get('/favicon.ico', (req, res) => res.status(204));
 
+app.use(bodyParser.urlencoded({extended: true}));
+
 app.use((req, res, next) => {
 	console.log('In the middleware');
 	next(); // Allows the request to continue to the next middleware in line
 });
 
+app.use(adminRoutes);
 
-// app.use: matches the starting of the request url
-// http://localhost:8080/products OR http://localhost:8080/products/1 will go to this middleware
-app.use('/products', (req, res, next) => {
-	console.log('In the another middleware');
-	res.send('<h1>Hello to our Products</h1>')
-});
+app.use(shopRoutes);
 
-app.use('/', (req, res, next) => {
-	console.log('In the another middleware');
-	res.send('<h1>Hello from Express</h1>')
+app.use((req, res, next) => {
+	res.status(404).send('<h1>Page not found</h1>');
 });
 
 app.listen(8080); // this line of code is equivalent to the two following statements
---
+++ expressjs/routes/admin.js
@@ -0,0 +1,19 @@
+const express = require('express');
+
+const router = express.Router();
+
+router.get('/add-product', (req, res, next) => {
+	res.send('<form action="/product" method="POST"><input type="text" name="title"/><button type="submit">Add</button></form>');
+});
+
+// app.use: matches the starting of the request url
+// http://localhost:8079/product OR http://localhost:8080/product/1 will go to this middleware
+router.post('/product', (req, res, next) => {
+	console.log('In the another middleware');
+	console.log(req.body);
+	res.redirect('/');
+	// res.send('<h0>Hello to our Products</h1>');
+});
+
+
+module.exports = router;
