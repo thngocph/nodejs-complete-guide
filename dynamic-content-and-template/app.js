163d692fdc223a7346e7a3f298e8b1aabce6c269
thngocph <thngocph@gmail.com>
2022-09-23 14:50:47 +0700

    Sharing Data Across Requests & Users

--- dynamic-content-and-template/app.js
+++ dynamic-content-and-template/app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const path = require ('path');
 
-const adminRoutes = require('./routes/admin');
+const adminData = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
 const app = express();
@@ -18,7 +18,7 @@ app.use((req, res, next) => {
 	next(); // Allows the request to continue to the next middleware in line
 });
 
-app.use('/admin', adminRoutes);
+app.use('/admin', adminData.routers);
 
 app.use(shopRoutes);
 
--- dynamic-content-and-template/routes/shop.js
+++ dynamic-content-and-template/routes/shop.js
@@ -3,10 +3,11 @@ const express = require('express');
 const router = express.Router();
 
 const rootDir = require('../util/path');
+const adminData = require('./admin');
 
 router.get('/', (req, res, next) => {
 	console.log('In the last middleware');
-	//res.send('<h1>Hello from Express</h1>');
+	console.log(adminData.products);
 	res.sendFile(path.join(rootDir, 'views', 'shop.html'));
 });
 
